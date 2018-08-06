ad020d343a6bb9505402a3dcaa5ac50d4faea561
Danyale <danyalecw@gmail.com>
2018-08-06 19:03:47 -0400

    heroku

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function gohome() {
+    window.location.href = "../Main/main.html"
+}
+
+// When the user scrolls down 95px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 95) {
+        document.getElementById("footer").style.display = "block";
+    } else {
+        document.getElementById("footer").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
