22a8c16e5f902cdc5314cbb8789343f3bd577b4a
Danyale <danyalecw@gmail.com>
2018-08-21 18:13:30 -0400

    rudolph object

--- script.js
+++ script.js
@@ -15,10 +15,3 @@ function scrollFunction() {
         document.getElementById("footer").style.display = "none";
     }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
